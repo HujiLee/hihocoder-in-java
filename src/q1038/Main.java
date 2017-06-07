0c354238be9eff853e85bb09645b38db6d094024
hujimiya <hujimiya@outlook.com>
2017-06-07 12:40:33 +0800

    TLE;试图用TreeMap替换数组惨遭失败;内存降下来一点点但是时间蹭地上去了1倍

--- src/q1038/Main.java
+++ src/q1038/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
+import java.util.TreeMap;
 
 /**
  * Created by Administrator on 2017/6/7 0007.
@@ -20,7 +21,7 @@ class Shouhin {
 
     @Override
     public String toString() {
-        return String.format("[need:%s,value:%s]",need,value);
+        return String.format("[need:%s,value:%s]", need, value);
     }
 }
 
@@ -29,18 +30,27 @@ class O1KnapsackSolver {
     private final int quantityOfShouken;
     private int[][] index_weight_maxValue;
 
+
     O1KnapsackSolver(Shouhin[] shouhins, int quantityOfShouken) {
         this.shouhins = shouhins;
         this.quantityOfShouken = quantityOfShouken;
-        index_weight_maxValue = new int[shouhins.length + 1][quantityOfShouken + 1];
+
+        need2MaxValueMaps = new TreeMap[shouhins.length + 1];
+        for (int i = 0; i < need2MaxValueMaps.length; i++) {
+            need2MaxValueMaps[i] = new TreeMap<Integer, Integer>();
+        }
+
+      /*  index_weight_maxValue = new int[shouhins.length + 1][quantityOfShouken + 1];
         for (int i = 1; i <= shouhins.length; i++) {
             for (int j = 1; j <= quantityOfShouken; j++) {
                 index_weight_maxValue[i][j] = -1;
             }
-        }
+        }*/
+
+
     }
 
-    private int get(int index, int weight) {
+/*    private int get(int index, int weight) {
         if (index_weight_maxValue[index][weight] < 0) {
             if (shouhins[index - 1].need > weight) {
                 index_weight_maxValue[index][weight] = get(index - 1, weight);
@@ -49,15 +59,45 @@ class O1KnapsackSolver {
             }
         }
         return index_weight_maxValue[index][weight];
+    }*/
+
+    private TreeMap<Integer, Integer>[] need2MaxValueMaps;
+
+    /**
+     * @param index  　表示第index个对象是否放入背包
+     * @param weight 表示该子问题中背包的容量
+     * @return 该子问题能得到的最大总价值
+     */
+    private int getByMap(int index, int weight) {
+        if (!need2MaxValueMaps[index].containsKey(weight)) {
+            if(index==0||weight==0){
+                need2MaxValueMaps[index].put(weight, 0);
+            }else{
+                if (weight < shouhins[index - 1].need) {
+                    need2MaxValueMaps[index].put(weight, getByMap(index - 1, weight));
+                } else {
+                    need2MaxValueMaps[index].put(weight, Math.max(
+                            shouhins[index - 1].value + getByMap(index - 1, weight - shouhins[index - 1].need),
+                            getByMap(index - 1, weight)
+                    ));
+                }
+            }
+
+        }
+        return need2MaxValueMaps[index].get(weight);
+
     }
 
-    public int getAnswer(){
-        return get(shouhins.length,quantityOfShouken);
+    public int getAnswer() {
+        return getByMap(shouhins.length, quantityOfShouken);
     }
 
 
 }
 
+/**
+ * @anchor https://hihocoder.com/problemset/problem/1038
+ */
 public class Main {
     static class Helper {
         static private StreamTokenizer streamTokenizer = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
@@ -75,10 +115,10 @@ public class Main {
         for (int i = 0; i < quantityOfShouhin; i++) {
             shouhins[i] = new Shouhin(Helper.getInt(), Helper.getInt());
         }
-        O1KnapsackSolver solver = new O1KnapsackSolver(shouhins,quantityOfShouken);
+        O1KnapsackSolver solver = new O1KnapsackSolver(shouhins, quantityOfShouken);
         System.out.println(solver.getAnswer());
 
 
     }
 }
-//AC	Java	9745ms	217MB
+//TLE	Java	17546ms	203MB
